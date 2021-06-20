8227114563d50ceffedcf6f4f64e241d5768f077
unryeong <leyng7@gmail.com>
2021-06-20 18:23:29 +0900

    Todo App - 프로젝트 구현

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo"/>
+    <span class="addContainer">
+      <font-awesome-icon class="addBtn" icon="plus" v-on:click="addTodo"/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoInput",
+  data: function () {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodoItem !== '') {
+        const obj = {
+          completed: false,
+          item: this.newTodoItem
+        };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
+    },
+    clearInput: function () {
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faPlus, faCheck, faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(faPlus, faCheck, faTrashAlt)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
